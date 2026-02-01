4e331ffc1e2768c973b62f00b719d20048b5a4f9
bad-antics <160459796+bad-antics@users.noreply.github.com>
2026-02-01 14:12:48 -0800

    feat: add performance monitoring utility

---
+++ performance-monitor.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+NullSec Performance Monitor v1.0
+Monitor and optimize module execution
+"""
+import time
+import psutil
+import json
+from datetime import datetime
+
+class PerformanceMonitor:
+    def __init__(self):
+        self.metrics = {}
+        self.start_time = None
+        
+    def start(self, module_name):
+        self.metrics[module_name] = {
+            'start': time.time(),
+            'cpu_start': psutil.cpu_percent(),
+            'mem_start': psutil.virtual_memory().percent
+        }
+        
+    def stop(self, module_name):
+        if module_name in self.metrics:
+            m = self.metrics[module_name]
+            m['end'] = time.time()
+            m['duration'] = m['end'] - m['start']
+            m['cpu_end'] = psutil.cpu_percent()
+            m['mem_end'] = psutil.virtual_memory().percent
+            
+    def report(self):
+        return json.dumps(self.metrics, indent=2)
+        
+    def save_report(self, path='performance_report.json'):
+        with open(path, 'w') as f:
+            f.write(self.report())
+
+if __name__ == '__main__':
+    monitor = PerformanceMonitor()
+    print("NullSec Performance Monitor v1.0")
